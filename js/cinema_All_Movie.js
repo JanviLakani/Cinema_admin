1fa9e1ad1054163adbbb8d964f3243349562bb37
JanviLakani <lakanijanvi1999@gmail.com>
2025-07-23 23:00:10 +0530

    add_h_w_23_07_cinema_all_movie_redirect_date_page

--- js/cinema_All_Movie.js
+++ js/cinema_All_Movie.js
@@ -1,11 +1,40 @@
 
-const handle_Cinema_Movie = () => {
-    // localStorage.setItem("cinemaId", id);   
+const handle_Cinema_Movie = async () => {
+  const cinema_movie = localStorage.getItem("cinemaId");
+  console.log("cinema_movie :-", cinema_movie);
 
-    const select_cinema_movie=localStorage.getItem("cinemaId");
+  try {
+    const response = await fetch("http://localhost:3000/Movie");
+    const allMovie = await response.json();
 
-    console.log(" select_cinema_movie new :- " ,  select_cinema_movie);
-    
+    const cinemaMovie = allMovie.filter((movie) => movie.cinema_id == cinema_movie);
+    console.log("Filter cinema movie....:", cinemaMovie);
+
+    let print = "";
+
+    cinemaMovie.map((movie) => {
+      print += `
+        <a href="#" class="cinema_card_img" onclick="handle_last_click('${movie.id}')">
+          <img src="images/movie_img/${movie.img_file}" />
+          <p>${movie.name}</p>
+        </a>
+      `;
+    });
+
+    document.getElementById("movie_list").innerHTML = print;
+
+  } catch (error) {
+    console.error("error :-", error);
+  }
+};
+
+const handle_last_click = (movieId) => {
+
+  localStorage.setItem("movieId", movieId);
+  console.log("movieId....." ,movieId);
+
+  window.location.href="user_select_date.html"
+  
 }
 
-window.onload=handle_Cinema_Movie;
+window.onload = handle_Cinema_Movie;
